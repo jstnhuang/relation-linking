0f742314125a3e7df93cce03fa5aca0a0c11d5c7
Justin Huang <jstn@cs.washington.edu>
2013-03-14 18:31:18 -0700

    Removed print statements and added exception handling in ReverbSrlRunner.

--- src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/ReverbSrlRunner.scala
+++ src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/ReverbSrlRunner.scala
@@ -24,8 +24,19 @@ object ReverbSrlRunner extends ScoobiApp {
   val clearParser = new ClearParser() 
   val clearSrl = new ClearSrl()
   
+  /**
+   * A Reverb extraction group contains an (arg1, rel, arg2) tuple, entity links and types for the
+   * args, a link for the relation, and a set of instances (sentences containing the tuple). For 
+   * each sentence of each group, this job constructs a partial sentence using the original text
+   * of (arg1, rel, arg2), and runs SRL on that to get the PropBank link. Then, it regroups the
+   * sentences back (in case not every sentence in the group had the same relation link). 
+   */
   def runSrl(groups: DList[String]): DList[String] = {
-    def combineStuff (key: String, lines: Iterable[String]) = {
+    /**
+     * Reducer -- the mapper emits groups with just one instance each, this combines the instances
+     * into just one group.
+     */
+    def combineInstances (key: String, lines: Iterable[String]) = {
       var firstGroup = ReVerbExtractionGroup.deserializeFromString(lines.head).get;
       var instances = firstGroup.instances
       
@@ -52,40 +63,42 @@ object ReverbSrlRunner extends ScoobiApp {
       ReVerbExtractionGroup.deserializeFromString(line) match {
         case Some(group) =>  {
           group.instances.flatMap { instance =>
-            val arg1Text = instance.extraction.arg1Text
-            val relText = instance.extraction.relText
-            val arg2Text = instance.extraction.arg2Text
-            val sentence = List(arg1Text, relText, arg2Text).mkString(" ")
-            System.out.println("sentence: " + sentence)
-            val graph = clearParser.dependencyGraph(sentence)
-            val frames = clearSrl(graph);
-            val relLink = if (frames.isEmpty) {
-              None
-            } else {
-              System.out.println("Frames: ")
-              frames map println
-              Some(frames(0).relation.toString())
+            try {
+              val arg1Text = instance.extraction.arg1Text
+              val relText = instance.extraction.relText
+              val arg2Text = instance.extraction.arg2Text
+              val sentence = List(arg1Text, relText, arg2Text).mkString(" ")
+              
+              val graph = clearParser.dependencyGraph(sentence)
+              val frames = clearSrl(graph);
+              val relLink = if (frames.isEmpty) {
+                None
+              } else {
+                Some(frames(0).relation.toString())
+              }
+              val updatedGroup = new ExtractionGroup(
+                group.arg1.norm,
+                group.rel.norm,
+                group.arg2.norm,
+                group.arg1.entity,
+                group.arg2.entity,
+                group.arg1.types,
+                group.arg2.types,
+                relLink,
+                Set(instance)
+              )
+              val key = List(
+                updatedGroup.arg1.norm,
+                updatedGroup.rel.norm,
+                updatedGroup.arg2.norm,
+                if (updatedGroup.arg1.hasEntity) updatedGroup.arg1.entity.get.fbid else "X",
+                if (updatedGroup.rel.hasLink) updatedGroup.rel.link else "X",
+                if (updatedGroup.arg2.hasEntity) updatedGroup.arg2.entity.get.fbid else "X"
+              ).mkString("-")
+              Some(key, ReVerbExtractionGroup.serializeToString(updatedGroup))
+            } catch {
+              case e: Exception => None
             }
-            val updatedGroup = new ExtractionGroup(
-              group.arg1.norm,
-              group.rel.norm,
-              group.arg2.norm,
-              group.arg1.entity,
-              group.arg2.entity,
-              group.arg1.types,
-              group.arg2.types,
-              relLink,
-              Set(instance)
-            )
-            val key = List(
-              updatedGroup.arg1.norm,
-              updatedGroup.rel.norm,
-              updatedGroup.arg2.norm,
-              if (updatedGroup.arg1.hasEntity) updatedGroup.arg1.entity.get.fbid else "X",
-              if (updatedGroup.rel.hasLink) updatedGroup.rel.link else "X",
-              if (updatedGroup.arg2.hasEntity) updatedGroup.arg2.entity.get.fbid else "X"
-            ).mkString("-")
-            Some(key, ReVerbExtractionGroup.serializeToString(updatedGroup))
           }
         }
         case None => {
@@ -94,7 +107,7 @@ object ReverbSrlRunner extends ScoobiApp {
         }
       }
     }.groupByKey
-    .map(keyValues => combineStuff(keyValues._1, keyValues._2))
+    .map(keyValues => combineInstances(keyValues._1, keyValues._2))
   }
   
   /**
