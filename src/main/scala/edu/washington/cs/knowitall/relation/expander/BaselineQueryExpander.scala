c4b5bb03dd1c9579b830f2eb196ad55e2b1ba43a
Justin Huang <jstn@cs.washington.edu>
2013-06-12 17:30:16 -0700

    Normalize baseline query rel.

--- src/main/scala/edu/washington/cs/knowitall/relation/expander/BaselineQueryExpander.scala
+++ src/main/scala/edu/washington/cs/knowitall/relation/expander/BaselineQueryExpander.scala
@@ -4,6 +4,7 @@ import edu.washington.cs.knowitall.relation.experiment.BenchmarkQuery
 import edu.washington.cs.knowitall.model.OpenIeQuery
 import edu.washington.cs.knowitall.model.QueryRel
 import edu.washington.cs.knowitall.model.QueryArg
+import edu.washington.cs.knowitall.relation.PhraseNormalizer
 
 object BaselineQueryExpander extends QueryExpander {
   override def getName() = "Baseline"
@@ -11,7 +12,7 @@ object BaselineQueryExpander extends QueryExpander {
   override def expandQuery(rawQuery: BenchmarkQuery): OpenIeQuery = {
     new OpenIeQuery(
       QueryArg.fromString(rawQuery.arg1.getOrElse("")),
-      QueryRel.fromString(rawQuery.rel.getOrElse("")),
+      QueryRel.fromString(PhraseNormalizer.normalize(rawQuery.rel.getOrElse(""))),
       QueryArg.fromString(rawQuery.arg2.getOrElse(""))
     )
   }
